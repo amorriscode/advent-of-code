d851d3d75f2ef45fa2238cb52f2ddf4bcc39b3c0
Anthony Morris <anthony@amorrissound.com>
2019-12-02 23:18:02 -0800

    Solve day 03

--- 2019/main.js
+++ 2019/main.js
@@ -3,10 +3,12 @@ import solve from './solve';
 // Import the days
 import dayOne from './01/index';
 import dayTwo from './02/index';
+import dayThree from './03/index';
 
 const solutions = {
     '01': dayOne,
     '02': dayTwo,
+    '03': dayThree,
 }
 
 const dayToSolve = process.argv[2];
