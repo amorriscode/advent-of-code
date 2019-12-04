d93f1d01ef978545297ef80966d39adadb8c2cfb
Anthony Morris <anthony@amorrissound.com>
2019-12-03 20:40:02 -0800

    Generate day 04

--- 2019/main.js
+++ 2019/main.js
@@ -1,14 +1,16 @@
 import solve from './solve';
 
-// Import the days
+// Import the days (TODO: make this automatic)
 import dayOne from './01/index';
 import dayTwo from './02/index';
 import dayThree from './03/index';
+import dayFour from './04/index';
 
 const solutions = {
     '01': dayOne,
     '02': dayTwo,
     '03': dayThree,
+    '04': dayFour,
 }
 
 const dayToSolve = process.argv[2];
