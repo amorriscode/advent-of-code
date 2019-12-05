90118a539b3b5b34230ef06d7c042a5a4ca5e445
Anthony Morris <anthony@amorrissound.com>
2019-12-05 01:03:00 -0800

    Solve Day 5 Part 2

--- 2019/05/index.js
+++ 2019/05/index.js
@@ -16,39 +16,48 @@ const run = (program, diagnosticCode) => {
         const paramMode2 = parseInt(instruction[1]);
         // const paramMode3 = parseInt(instruction[0]);
 
-        let param1;
-        let param2;
-        let param3;
+        const param1 = getParam(paramMode1, position + 1);
+        const param2 = getParam(paramMode2, position + 2);
+        const param3 = program[position + 3];
+
         switch (opCode) {
             case 1:
-                param1 = getParam(paramMode1, position + 1)
-                param2 = getParam(paramMode2, position + 2)
-                param3 = program[position + 3];
-
                 program[param3] = param1 + param2;
-
                 position += 4;
                 break;
             case 2:
-                param1 = getParam(paramMode1, position + 1)
-                param2 = getParam(paramMode2, position + 2)
-                param3 = program[position + 3];
-
                 program[param3] = param1 * param2;
-
                 position += 4;
                 break;
             case 3:
-                param1 = program[position + 1];
-
-                program[param1] = diagnosticCode;
-
-                position += 2;
+                program[program[position + 1]] = diagnosticCode;
+                position += 2;  
                 break;
             case 4:
-                output = getParam(paramMode1, position + 1);
-
+                output = param1;
                 position += 2;
+                break;
+            case 5:
+                if (param1 !== 0) {
+                    position = param2;
+                } else {
+                    position += 3;
+                }
+                break;
+            case 6:
+                if (param1 === 0) {
+                    position = param2;
+                } else {
+                    position += 3;
+                }
+                break;
+            case 7:
+                program[param3] = param1 < param2 ? 1 : 0;
+                position += 4;
+                break;
+            case 8:
+                program[param3] = param1 === param2 ? 1 : 0;
+                position += 4;
                 break;
         }
     }
@@ -58,12 +67,16 @@ const run = (program, diagnosticCode) => {
 
 const part1 = () => {
     const input = getInput();
-    console.log(run(input, 1));
+    const answer = run(input, 1);
+
+    return `The diagnostic code is ${answer}`;
 }
 
 const part2 = () => {
     const input = getInput();
-    run(input);
+    const answer = run(input, 5);
+
+    return `The diagnostic code for system ID 5 is ${answer}`;
 }
 
 export default {
