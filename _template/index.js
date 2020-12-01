2e934b203ab3d1f4223e59190403cd88a2d7d6e6
Anthony Morris <anthony@amorrissound.com>
2020-11-30 21:47:09 -0800

    Reformat template files

--- _template/index.js
+++ _template/index.js
@@ -1,23 +1,22 @@
-import dedent from 'dedent';
-import colors from 'colors';
-import input from './input';
+import dedent from 'dedent'
+import colors from 'colors'
+import input from './input'
 
-const getInput = () => input.split('').map(Number);
+const getInput = () => input.split('').map(Number)
 
-const run = () => {};
+const run = () => {}
 
 const part1 = () => {
-    const input = getInput();
-    run(input);
+  const input = getInput()
+  run(input)
 }
 
 const part2 = () => {
-    const input = getInput();
-    run(input);
+  const input = getInput()
+  run(input)
 }
 
 export default {
-    part1,
-    part2,
+  part1,
+  part2,
 }
-
