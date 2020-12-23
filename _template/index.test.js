3489caca34fac2cb64dd6fde2272c624776fb66b
Anthony Morris <anthony@amorrissound.com>
2020-12-23 12:03:47 -0800

    Make template more generic and use an input parsing function

--- _template/index.test.js
+++ _template/index.test.js
@@ -1,8 +1,10 @@
-const { part1, part2 } = require('./index.js')
+const { inputParser, part1, part2 } = require('./index.js')
 const dedent = require('dedent')
 
 test('part1 passes examples', () => {
-  expect(part1(dedent``.split('\n\n'))).toBe(0)
+  expect(part1(inputParser(dedent``))).toBe(0)
 })
 
-test('part2 passes examples', () => {})
+test('part2 passes examples', () => {
+  expect(part2(inputParser(dedent``))).toBe(0)
+})
